82dd55b96d1c3134875eb412fcef6efbde4b1f57
Shubham Agrawal <gr8shubham07@gmail.com>
2025-04-17 22:02:43 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -9,8 +9,16 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+       console.log(`Server is running on port ${process.env.PORT || 8000}`); 
+    })
+})
 
+.catch((err)=>{
+    console.log("Error connecting to MongoDB:", err);
+})
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        error = [],
+        statck = ""
+    ){
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message,
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = this.errors
+
+        if (stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export {ApiError};
